239cdf8cb60eceaea6382889662fcc65e4823358
dbzerjav <dbzerjav@gmail.com>
2016-04-08 18:25:33 +0000

    create show action and add gravitar image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravitar_for(user, options = {size: 80})
+    gravitar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravitar_url = "https://secure/gravitar.com/avatar/#{gravitar_id}?s=#{size}"
+    image_tag(gravitar_url, alt: user.username, class: "img-circle")
+  end
 end
