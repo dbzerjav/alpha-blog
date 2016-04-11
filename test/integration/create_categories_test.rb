83f62721221fe05eb21f7d3be1f798198ee83913
dbzerjav <dbzerjav@gmail.com>
2016-04-11 07:25:01 +0000

    fix tests and update navbar with categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,7 +2,12 @@ require "test_helper"
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
   
+  def setup
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+  end
+  
   test "get new category form and create category" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template "categories/new"
     assert_difference "Category.count", 1 do
@@ -13,6 +18,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
   
   test "invalid category submission results in failure" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template "categories/new"
     assert_no_difference "Category.count" do
@@ -25,7 +31,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   
   test "should redirect create when admin not logged in" do
     assert_no_difference "Category.count" do
-      post :create, category: { name: "sports" }
+      post categories_path, category: {name: "sports"}
     end
     assert_redirected_to categories_path
   end
